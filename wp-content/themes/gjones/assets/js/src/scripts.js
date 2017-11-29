421b2a182913578439ed3c9c2d1498e8e60f1238
Grahame Jones <grahame@drivedigital.ca>
2017-11-29 13:08:22 -0800

    added course list

--- wp-content/themes/gjones/assets/js/src/scripts.js
+++ wp-content/themes/gjones/assets/js/src/scripts.js
@@ -22,16 +22,15 @@
         ]
     };
 
-    //console.log(jsonData);
+    
     var fuse = new Fuse(jsonData, options);
-    //console.log(fuse);
+    //console.log(fuse); 
 
 
     /*
      * Form handler for Search Courses
      */
     var searchForm = $('#search-courses');
-
 
     searchForm.find('input[name=search-courses]').on('keyup', function(e){
 
@@ -50,18 +49,23 @@
             
             if(result){
                 if (result.length < 100){
-
+      
                     $.each(result, function(){
+                        var des = this.item.description;
+                        des = fixhtml(des.substring(0, 300)+'...');
+
                         var o = '';
-                        o += '<div class="course grid__item one-quarter" id="'+this.item.id+'">';
+                        o += '<div class="course" id="'+this.item.id+'">';
                             o += '<div class="course__item">';
                             o += '<h2 class="ct">'+this.item.title+'</h2>';
-                            o += '<p class="cd">'+this.item.description+'</p>';
+                            o += '<p class="cd">'+des+'</p>';
+                         /*
+                            if ( this.item.image ){
+                                o += '<img class="course__image" src="'+this.item.image+'"/>';  
+                            }
+                        */
                             if ( this.item.url ){
                                 o += '<a target="_blank" class="btn" href="'+this.item.url+'">View Course</a>';
-                            }
-                            if ( this.item.image ){
-                              o += '<img class="course__image" src="'+this.item.image+'"/>';  
                             }
                             if ( this.item.video ){
                                 o += '<a target="_blank" class="btn btn--secondary" href="'+this.item.video+'">Watch Video</a>';
@@ -69,18 +73,86 @@
                             o += '</div>';
                         o += '</div>';
                         
+                        displayResults.addClass('updated');
                         displayResults.prepend(o);
                     });
                 }
             }
         }, 100);
-
-
-
-
     });
  
 
+
+    /*
+    * Click handler to add Course to user list.
+    */
+    var courseList = [];
+    var o = '';
+
+    $('#search-results').on('click', '.course', function(){
+        $this = $(this);
+        courseTitle = $this.find('.ct').html();
+
+        if ( $this.hasClass('course__added') ){
+            $this.removeClass('course__added');
+            courseList = removeCourseFromList(courseList, '<li>'+courseTitle+'</li>');
+            $('.course-list').html(courseList);
+        }else{
+            $this.addClass('course__added');
+            if (courseList.indexOf(courseTitle) == -1){
+                courseList.push('<li>'+courseTitle+'</li>');
+            }
+            $('.course-list').html(courseList);
+        }
+
+
+    });  
+
+
+
+    /*
+
+        Click on course.
+            Add ID to list array
+        
+        Click on course again
+            Remove from list array by ID
+        
+        always
+            re-render html based on array.
+
+    */
+
+    
+  //o = '<span class="course-list__item" id="item-'+$this.attr('id')+'"><span class="course-list__remove">x</span><a href="#'+$this.attr('id')+'">'+$this.find('.ct').html()+'</a></span>';
+
+    // Adds a course to the courseList array.
+    function removeCourseFromList(courseList, course){
+        for(var i in courseList){
+            if(courseList[i] == course){
+                courseList.splice(i, 1);
+                break;
+            }
+        }       
+        return courseList;
+    }
+
+
+
+/*
+// TODO: Add selected items to hidden field and attach during form submit.
+    var courseList = $('#selected_courses').val();
+    courseList += $this.attr('id')+',';
+    $('#selected_courses').val(courseList);
+*/
+        
+ 
+
+    function fixhtml(html){
+        var div = document.createElement('div');
+        div.innerHTML=html;
+        return (div.innerHTML);
+    }
 
 
 })( jQuery );
--- wp-content/themes/gjones/template-add-posting.php
+++ wp-content/themes/gjones/template-add-posting.php
@@ -51,31 +51,7 @@ $context['cachedData'] = str_replace('}}', '}]', $context['cachedData']);
 
 
 
-/*
-    Load Postings of current user only.
-*/
-$args = array(
-    'post_type'              => 'posting',
-    'author'                 => $user->ID,
-    'post_status'            => 'publish',
-    'order'                  => 'ASC',
-    'orderby'                => 'title',
-    'posts_per_page'         => 10,
-    'posts_per_archive_page' => 10,
-    'nopaging'               => false,
-    'paged'                  => get_query_var( 'paged' ),
-    //'no_found_rows'          => false,
-    //'cache_results'          => true,
-    //'update_post_term_cache' => true,
-    //'update_post_meta_cache' => true,
-);
-$query = new WP_Query( $args );
 
-// Load acf fields
-foreach ($query->posts as $k=>$post) {
-    $post->acf = get_fields($post->ID);
-}
-$context['posts'] = $query->posts;
 
 
 Timber::render('page-add-posting.twig', $context);
